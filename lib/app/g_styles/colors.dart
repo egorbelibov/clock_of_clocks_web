1ffa155f5e7906c5f89a21279a7c9c1d552d6ed4
Egor Belibov <egor@belibov.dev>
2020-01-03 14:50:50 +0000

    Add gradient colors & [gradients.dart]

--- lib/app/g_styles/colors.dart
+++ lib/app/g_styles/colors.dart
@@ -5,17 +5,23 @@ enum PaletteColor {
   secondaryColor,
   tertiaryColor,
   backgroundColor,
+  primaryGradientColor,
+  secondaryGradientColor,
 }
 
 const lightPrimaryColor = Color(0xFF000000);
 const lightSecondaryColor = Color(0x20252525);
 const lightTertiaryColor = Color(0xFFFE1212);
 const lightBackgroundColor = Color(0xFFFFFFFF);
+const lightPrimaryGradientColor = Color(0xFFFFFFFF);
+const lightSecondaryGradientColor = Color(0xFF000000);
 
 const darkPrimaryColor = Color(0xFFFFFFFF);
 const darkSecondaryColor = Color(0x20DADADA);
 const darkTertiaryColor = Color(0xFFFE1212);
 const darkBackgroundColor = Color(0xFF000000);
+const darkPrimaryGradientColor = Color(0xFF000000);
+const darkSecondaryGradientColor = Color(0xFFFFFFFF);
 
 Color themeBasedColor(BuildContext context, PaletteColor color) {
   final isLightTheme = Theme.of(context).brightness == Brightness.light;
@@ -28,6 +34,15 @@ Color themeBasedColor(BuildContext context, PaletteColor color) {
       return isLightTheme ? lightTertiaryColor : darkTertiaryColor;
     case PaletteColor.backgroundColor:
       return isLightTheme ? lightBackgroundColor : darkBackgroundColor;
+    case PaletteColor.primaryGradientColor:
+      return isLightTheme
+          ? lightPrimaryGradientColor
+          : darkPrimaryGradientColor;
+    case PaletteColor.secondaryGradientColor:
+      return isLightTheme
+          ? lightSecondaryGradientColor
+          : darkSecondaryGradientColor;
+
     default:
       return isLightTheme ? Colors.black : Colors.white;
   }
