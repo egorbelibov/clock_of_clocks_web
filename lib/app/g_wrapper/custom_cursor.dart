9ec4c370dbb7e4cc21bb40e2cfd500f4afaec9a6
Egor Belibov <egor@belibov.dev>
2020-01-04 20:58:34 +0000

    Add [CustomCursor] wrapper and update web/index.html
    This is a work-around while flutter doesn't have something
    default to do this... Right now this and other things just
    start firing a bunch of asserts... so it's kinda untestable...

---
+++ lib/app/g_wrapper/custom_cursor.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/gestures.dart';
+import 'package:flutter/widgets.dart';
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
+class CustomCursor extends MouseRegion {
+  static final appContainer =
+      html.window.document.getElementById('app-container');
+
+  // cursor types from http://www.javascripter.net/faq/stylesc.htm
+  static final String pointer = 'pointer';
+  static final String text = 'text';
+
+  CustomCursor({Widget child, String cursorStyle = 'pointer'})
+      : super(
+            onHover: (PointerHoverEvent evt) {
+              if (kIsWeb) {
+                appContainer.style.cursor = cursorStyle;
+              }
+            },
+            onExit: (PointerExitEvent evt) {
+              if (kIsWeb) {
+                appContainer.style.cursor = 'default';
+              }
+            },
+            child: child);
+}
