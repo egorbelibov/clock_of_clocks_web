39d14bfbf981f1ddf350f51bdd1e34c938e80c00
Egor Belibov <egor@belibov.dev>
2020-01-04 16:15:37 +0000

    Add favicon.ico (Web, Android, iOS) and update index.html

--- lib/app/screens/home_screen/home_screen.dart
+++ lib/app/screens/home_screen/home_screen.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+    return WillPopScope(
+      onWillPop: () {
+        return; // Don't allow screen to pop()
+      },
+      child: Scaffold(
+        body: Center(
+          child: Text('Home Screen'),
+        ),
       ),
     );
   }
