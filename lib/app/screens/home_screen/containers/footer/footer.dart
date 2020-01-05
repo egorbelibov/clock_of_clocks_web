8d70e11d290d921391b0692d4b429cd54bc5aea1
Egor Belibov <egor@belibov.dev>
2020-01-05 17:45:26 +0000

    Make [Footer] mobile & dekstop friendly

--- lib/app/screens/home_screen/containers/footer/footer.dart
+++ lib/app/screens/home_screen/containers/footer/footer.dart
@@ -1,76 +1,27 @@
-import 'package:clock_of_clocks_website/app/g_styles/spaces.dart';
 import 'package:flutter/material.dart';
 
-import '../../../../g_helpers/links.dart';
-import '../../../../g_styles/colors.dart';
-import '../../../../g_wrapper/custom_cursor.dart';
-import 'styles.dart';
+import 'desktop/desktop_footer.dart';
+import 'mobile/mobile_footer.dart';
 
-class Footer extends StatelessWidget {
+class Footer extends StatefulWidget {
+  @override
+  _FooterState createState() => _FooterState();
+}
+
+class _FooterState extends State<Footer> {
+  Widget desktopFooter;
+  Widget mobileFooter;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: topScreenPadding, horizontal: rightScreenPadding),
-      color: themeBasedColor(context, PaletteColor.footerColor),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          _leftColumn(context),
-          _rightColumn(context),
-        ],
-      ),
-    );
-  }
-
-  Widget _leftColumn(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomCursor(
-          cursorStyle: CustomCursor.text,
-          child: SelectableText(
-            'Designed & Developed by',
-            style: defaultTextStyle(context),
-          ),
-        ),
-        CustomCursor(
-          cursorStyle: CustomCursor.pointer,
-          child: GestureDetector(
-            onTap: () => openWebUrl('https://twitter.com/egorbelibov'),
-            child: Text(
-              'Egor Belibov',
-              style: remarkedTextStyle(context),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _rightColumn(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        CustomCursor(
-          cursorStyle: CustomCursor.text,
-          child: SelectableText(
-            'Project Source Code',
-            style: remarkedTextStyle(context),
-          ),
-        ),
-        CustomCursor(
-          cursorStyle: CustomCursor.pointer,
-          child: GestureDetector(
-            onTap: () {
-              openWebUrl('https://github.com/egorbelibov/clock_of_clocks');
-            },
-            child: Text(
-              'github.com/egorbelibov/clock_of_clocks',
-              style: defaultTextStyle(context),
-            ),
-          ),
-        ),
-      ],
-    );
+    return LayoutBuilder(builder: (_, constraints) {
+      if (constraints.maxWidth > 1400) {
+        desktopFooter ??= DesktopFooter();
+        return desktopFooter;
+      } else {
+        mobileFooter ??= MobileFooter();
+        return mobileFooter;
+      }
+    });
   }
 }
---
+++ lib/app/screens/home_screen/containers/footer/mobile/mobile_footer.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../g_helpers/links.dart';
+import '../../../../../g_styles/colors.dart';
+import '../../../../../g_styles/spaces.dart';
+import '../../../../../g_wrapper/custom_cursor.dart';
+import '../styles.dart';
+
+class MobileFooter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: topScreenPadding,
+        horizontal: rightScreenPadding,
+      ),
+      color: themeBasedColor(context, PaletteColor.footerColor),
+      child: Center(
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomCursor(
+              cursorStyle: CustomCursor.text,
+              child: SelectableText(
+                'Designed & Developed by',
+                style: defaultTextStyle(context),
+              ),
+            ),
+            CustomCursor(
+              cursorStyle: CustomCursor.pointer,
+              child: GestureDetector(
+                onTap: () => openWebUrl('https://twitter.com/egorbelibov'),
+                child: Text(
+                  'Egor Belibov',
+                  style: remarkedTextStyle(context),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
