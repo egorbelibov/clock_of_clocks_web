2297b5b5e642e18c486e96f426ad2daeb7dc8886
Egor Belibov <egor@belibov.dev>
2020-01-05 12:08:37 +0000

    Add [ProjectTag] to [HomeScreen] -> [Content]
    - Also, initialize [ProjectInformation] container.
    - And a slight modification to footer.dart

---
+++ lib/app/screens/home_screen/containers/content/components/project_tag/project_tag.dart
@@ -0,0 +1,70 @@
+import 'package:clock_of_clocks_website/app/g_styles/colors.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../g_helpers/links.dart';
+import '../../../../../../g_styles/text_styles.dart';
+import '../../../../../../g_wrapper/custom_cursor.dart';
+import 'styles.dart';
+
+class ProjectTag extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 25,
+      right: 45,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          tagText(context),
+          SizedBox(width: 15),
+          tagDecoration(context),
+        ],
+      ),
+    );
+  }
+}
+
+Widget tagText(BuildContext context) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: [
+      CustomCursor(
+        cursorStyle: CustomCursor.text,
+        child: SelectableText(
+          'Created for the',
+          style: defaultTextStyle(context),
+        ),
+      ),
+      CustomCursor(
+        cursorStyle: CustomCursor.pointer,
+        child: GestureDetector(
+          onTap: () => openWebUrl('https://flutter.dev/clock'),
+          child: Text(
+            'Flutter Clock Challenge',
+            style: remarkedTextStyle(context),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget tagDecoration(BuildContext context) {
+  const double containerWidth = 10;
+  const double containerHeight = 35;
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [
+      Container(
+        width: containerWidth,
+        height: containerHeight,
+        color: themeBasedColor(context, PaletteColor.primaryColor),
+      ),
+      Container(
+        width: containerWidth,
+        height: containerHeight,
+        color: themeBasedColor(context, PaletteColor.tertiaryColor),
+      )
+    ],
+  );
+}
---
+++ lib/app/screens/home_screen/containers/content/containers/project_information/project_information.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+/// Introduction to Project and Action
+class ProjectInformation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
