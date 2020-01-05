c08c0b15ac74cad368a82f300664f799d8fbc73d
Egor Belibov <egor@belibov.dev>
2020-01-05 19:59:47 +0000

    Update [Footer] desktop and mobile sizes (it's too big...)

--- lib/app/screens/home_screen/containers/footer/mobile/styles.dart
+++ lib/app/screens/home_screen/containers/footer/mobile/styles.dart
@@ -1,22 +1,20 @@
+import 'package:clock_of_clocks_website/app/g_styles/colors.dart';
 import 'package:clock_of_clocks_website/app/g_styles/fonts.dart';
 import 'package:flutter/widgets.dart';
-
-import '../../../../g_styles/colors.dart';
 
 TextStyle defaultTextStyle(BuildContext context) {
   return TextStyle(
     fontFamily: defaultFontFamily,
-    fontSize: 16,
+    fontSize: 15,
     fontWeight: FontWeight.w300,
     color: themeBasedColor(context, PaletteColor.footerTextColor),
   );
 }
 
-
 TextStyle remarkedTextStyle(BuildContext context) {
   return TextStyle(
     fontFamily: defaultFontFamily,
-    fontSize: 16,
+    fontSize: 15,
     fontWeight: FontWeight.w700,
     color: themeBasedColor(context, PaletteColor.footerTextColor),
   );
