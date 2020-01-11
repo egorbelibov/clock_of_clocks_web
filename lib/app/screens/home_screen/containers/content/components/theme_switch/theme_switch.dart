76c3181b9ae4014988e0b165ae70df84c48fb13c
Egor Belibov <egor@belibov.dev>
2020-01-11 22:11:10 +0000

    Temporary fix for filled flutter issue: #48631

--- lib/app/screens/home_screen/containers/content/components/theme_switch/theme_switch.dart
+++ lib/app/screens/home_screen/containers/content/components/theme_switch/theme_switch.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:property_change_notifier/property_change_notifier.dart';
 
 import '../../../../../../g_styles/colors.dart';
-import '../../../../../../g_styles/spaces.dart';
 import '../../../../../../g_wrapper/custom_cursor.dart';
 
 class ThemeSwitch extends StatefulWidget {
@@ -43,10 +42,10 @@ class _ClockFrameState extends State<ThemeSwitch> {
 
   @override
   Widget build(BuildContext context) {
-    return _renderSwitch();
+    return _buildSwitch();
   }
 
-  Widget _renderSwitch() {
+  Widget _buildSwitch() {
     final primaryColor = isLightTheme(_brightness)
         ? lightThemePrimaryColor
         : darkThemePrimaryColor;
@@ -57,17 +56,18 @@ class _ClockFrameState extends State<ThemeSwitch> {
       end: rotationAngle,
     );
 
-    return Align(
-      alignment: Alignment.topLeft,
+    return Positioned(
+      top: 0,
+      left: 0,
       child: Padding(
         padding: EdgeInsets.only(
-          top: topScreenPadding,
+          top: 20,
           left: 20,
         ),
         child: TweenAnimationBuilder(
           tween: rotationAngleTween,
           duration: Duration(milliseconds: 500),
-          curve: Curves.easeInOut,
+          curve: Curves.decelerate,
           builder: (_, angle, child) {
             return Transform.rotate(
               angle: angle,
@@ -78,20 +78,18 @@ class _ClockFrameState extends State<ThemeSwitch> {
             cursorStyle: CustomCursor.pointer,
             child: GestureDetector(
               onTap: () => _updateThemeBrightness(),
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 500),
-                curve: Curves.easeInOut,
+              child: Container(
                 width: 35,
                 height: 35,
                 decoration: BoxDecoration(
                   color: Color(0xFFFFFFFF),
-                  shape: BoxShape.circle,
+                  // shape: BoxShape.circle,
                   border: Border.all(
                     width: 3,
                     color: primaryColor,
                   ),
                 ),
-                child: _halfCircle(),
+                child: _buildHalfCircle(),
               ),
             ),
           ),
@@ -100,16 +98,17 @@ class _ClockFrameState extends State<ThemeSwitch> {
     );
   }
 
-  Widget _halfCircle() {
+  Widget _buildHalfCircle() {
     return FractionallySizedBox(
       alignment: Alignment.centerRight,
       widthFactor: 0.50,
       child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(13.5),
-            bottomRight: Radius.circular(13),
-          ),
+          // FILED FLUTTER ISSUE: #48631
+          // borderRadius: BorderRadius.only(
+          //   topRight: Radius.circular(13.5),
+          //   bottomRight: Radius.circular(13),
+          // ),
           color: Color(0xFF000000),
         ),
       ),
