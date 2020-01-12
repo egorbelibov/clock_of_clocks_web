244208b273669c447d22a154f78630f841fe1cc2
Egor Belibov <mail@egorb.dev>
2020-01-12 11:42:52 +0000

    Add copyright headers

--- lib/app/g_animations/funny_pulsing_squares/funny_pulsing_squares.dart
+++ lib/app/g_animations/funny_pulsing_squares/funny_pulsing_squares.dart
@@ -1,3 +1,7 @@
+// Copyright 2019 Egor Belibov. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 import 'package:flutter/widgets.dart';
 
 class FunnyPulsingSquares extends StatefulWidget {
