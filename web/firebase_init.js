6044895d88e7bf3d63b6c9c2fc4653523fcb6856
Egor Belibov <egor@belibov.dev>
2020-01-17 15:16:33 +0000

    Add robots.txt (Lighouse complaining) + Firebase adjustment
    - TODO: File issue @Flutter about this... Should probably
    add this to documentation / new file... (A robots.txt
    file is always invalid if it doesn't exist in the project
    and the website is redirected to the main index.html file,
    which of course is an invalid robots.txt file).
    
    - Also replace nonsense firebase links in html... replace
    them with cdn and create simple javascript that initializes
    firebase instance.

---
+++ web/firebase_init.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+	apiKey: "AIzaSyDx451YIKIgNPUutI7QiKY5L4AsT-TIN0Q",
+	authDomain: "clockofclocks.firebaseapp.com",
+	databaseURL: "https://clockofclocks.firebaseio.com",
+	projectId: "clockofclocks",
+	storageBucket: "clockofclocks.appspot.com",
+	messagingSenderId: "355649369409",
+	appId: "1:355649369409:web:bd4dc8f2060641c0cdddc0",
+	measurementId: "G-MTNLYEZF6N"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
